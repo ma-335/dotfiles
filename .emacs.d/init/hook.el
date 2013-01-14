96416e5a7f438c1b9bfba7476d27dfda07c874b6
Masaori Koshiba <masaori335@gmail.com>
2013-01-15 00:40:03 +0900

    Set tab-width 2 in sh-mode

--- .emacs.d/init/hook.el
+++ .emacs.d/init/hook.el
@@ -1,7 +1,13 @@
 (add-hook 'lisp-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; delete trailing whitespace
-(add-hook 'c-mode-hook (lambda () (add-to-list 'wtite-file-functions 'delete-trailing-whitespace)))
-(add-hook 'ruby-mode-hook (lambda () (add-to-list 'wtite-file-functions 'delete-trailing-whitespace)))
-(add-hook 'clojure-mode-hook (lambda () (add-to-list 'wtite-file-functions 'delete-trailing-whitespace)))
-(add-hook 'scheme-mode-hook (lambda () (add-to-list 'wtite-file-functions 'delete-trailing-whitespace)))
+;;; sh-mode
+(defun setup-sh-mode-indent ()
+  "My own personal preferences for `sh-mode'."
+  (interactive)
+  (setq sh-basic-offset 2
+        sh-indentation 2
+        sh-indent-for-case-label 0
+        sh-indent-for-case-alt '+))
+
+(add-hook 'sh-mode-hook 'setup-sh-mode-indent)

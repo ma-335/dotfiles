3f20b37d7e7e69d7e502a2f179473cc5f420c95d
Masaori Koshiba <masaori335@gmail.com>
2014-11-30 18:03:47 +0900

    Change background color of region

--- .emacs.d/init/face.el
+++ .emacs.d/init/face.el
@@ -1,7 +1,4 @@
 (load-theme 'zenburn t)
 
 (set-face-background 'default "#000000")
-(set-face-background 'region "#3333333")
-
-;; (require 'powerline)
-;; (powerline-default-theme)
+(set-face-background 'region "#555555")

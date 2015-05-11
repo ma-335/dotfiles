5fd709a9797052b30475d57c2e501271e480a589
Masaori Koshiba <masaori335@gmail.com>
2015-05-11 21:32:25 +0900

    Add settings for javascript

--- .emacs.d/init/mode.el
+++ .emacs.d/init/mode.el
@@ -1,6 +1,18 @@
 (setq auto-mode-alist (append '(("\\.ml$" . tuareg-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("Rakefile$" . ruby-mode)) auto-mode-alist))
-
+(setq auto-mode-alist (append '(("\\.md$" . markdown-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.html$" . web-mode)) auto-mode-alist))
 
 (setq fill-column 100)
 (setq rst-mode-hook 'turn-on-auto-fill)
+
+(setq js-indent-level 2)
+
+(add-hook 'makefile-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode)
+            (setq tab-width 4)))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode)
+            (setq tab-width 4)))
